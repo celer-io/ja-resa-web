fe47811062f9901cda99def2234151001e0b77cd
Clement Lepissier <clem.lepissier@gmail.com>
2017-10-24 04:30:55 +0200

    config fullcalendar and start implementation of edit modal

--- src.js
+++ src.js
@@ -1,26 +1,46 @@
 $(document).ready(function() {
-    console.log("heyyyyysssssssssssse")
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
+      businessHours: [
+        {
+          dow: [ 1, 2, 3, 4, 5, 6 ],
+          start: '10:00',
+          end: '23:00'
+        },
+        {
+          dow: [ 0 ],
+          start: '14:00',
+          end: '20:00'
+        }
+      ],
+      allDaySlot: false,
+      height: 725,
+      minTime: '10:00',
+      maxTime: '23:00',
+      defaultView: 'agendaWeek',
+      dayClick: openEditForm,
       events: [
         {
-          title  : 'event1',
-          start  : '2017-10-01'
+          title  : 'truc',
+          start  : '2017-10-27T12:00:00',
+          end  : '2017-10-27T14:00:00'
         },
         {
-          title  : 'event2',
-          start  : '2017-10-02',
-          end    : '2017-10-03'
-        },
-        {
-          title  : 'event3',
-          start  : '2017-10-02T12:30:00',
-          end  : '2017-10-02T13:30:00',
-          allDay : false // will make the time show
+          title  : 'machin',
+          start  : '2017-10-27T14:00:00',
+          end  : '2017-10-27T16:00:00'
         }
       ]
-        // put your options and callbacks here
     })
 
 });
+
+function openEditForm(date) {
+  $('#edit-form').toggleClass('is-active')
+  console.log('date :', date)
+}
+
+function closeEditForm() {
+  $('#edit-form').toggleClass('is-active')
+}
